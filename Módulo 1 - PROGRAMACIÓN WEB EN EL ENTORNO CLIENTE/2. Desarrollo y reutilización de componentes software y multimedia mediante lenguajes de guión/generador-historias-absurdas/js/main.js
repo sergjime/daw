74156088c875491c71fc609c6f0079ff359efc10
Sergio <sergjime@gmail.com>
2021-11-16 14:54:08 +0100

    Generador de historias absurdas

--- Módulo 1 - PROGRAMACIÓN WEB EN EL ENTORNO CLIENTE/2. Desarrollo y reutilización de componentes software y multimedia mediante lenguajes de guión/generador-historias-absurdas/js/main.js
+++ Módulo 1 - PROGRAMACIÓN WEB EN EL ENTORNO CLIENTE/2. Desarrollo y reutilización de componentes software y multimedia mediante lenguajes de guión/generador-historias-absurdas/js/main.js
@@ -2,12 +2,8 @@ const customName = document.getElementById("customname");
 const randomize = document.querySelector(".randomize");
 const story = document.querySelector(".story");
 
-function randomValueFromArray(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-
 const storyText =
-  "Hacía 94 farenheit afuera, así que: insertx: salí a caminar. Cuando llegaron a: inserty :, miraron con horror por unos momentos, luego: insertz :. Bob vio todo, pero no se sorprendió: insertx: pesa 300 libras y era un día caluroso";
+  "Hacía 94 farenheit afuera, así que : insertx: salí a caminar. Cuando llegaron a : inserty:, miraron con horror por unos momentos, luego : insertz:. Bob vio todo, pero no se sorprendió : insertx: pesa 300 libras y era un día caluroso";
 const insertX = ["Willy Wonka", "Big Daddy", "Papá Noel"];
 const insertY = ["el comedor de beneficencia", "Disneyland", "la Casa Blanca"];
 const insertZ = [
@@ -16,31 +12,42 @@ const insertZ = [
   "convertido en una babosa y arrastrándose",
 ];
 
+customName.focus();
+
 randomize.addEventListener("click", result);
 
+function randomValueFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 function result() {
-  let newStory = storyText;
-  let xitem = randomValueFromArray(insertX);
-  let yitem = randomValueFromArray(insertY);
-  let zitem = randomValueFromArray(insertZ);
+  if (customName.value === "") {
+    alert("No escribiste nada en el campo");
+    customName.focus();
+  } else {
+    let newStory = storyText;
+    let xitem = randomValueFromArray(insertX);
+    let yitem = randomValueFromArray(insertY);
+    let zitem = randomValueFromArray(insertZ);
 
-  newStory = newStory.replace(": insertx:", xitem);
-  newStory = newStory.replace(": inserty:", yitem);
-  newStory = newStory.replace(": insertz:", zitem);
-  newStory = newStory.replace(": insertx:", xitem);
+    newStory = newStory.replace(": insertx:", xitem);
+    newStory = newStory.replace(": inserty:", yitem);
+    newStory = newStory.replace(": insertz:", zitem);
+    newStory = newStory.replace(": insertx:", xitem);
 
-  if (customName.value !== "") {
-    let name = customName.value;
-    newStory = newStory.replace("Bob", name);
+    if (customName.value !== "") {
+      let name = customName.value;
+      newStory = newStory.replace("Bob", name);
+    }
+
+    if (document.getElementById("uk").checked) {
+      let weight = Math.round(300);
+      let temperature = Math.round(94);
+      newStory = newStory.replace("300 pounds", weight);
+      newStory = newStory.replace("94 farenheit", temperature);
+    }
+
+    story.textContent = newStory;
+    story.style.visibility = "visible";
   }
-
-  if (document.getElementById("uk").checked) {
-    let weight = Math.round(300);
-    let temperature = Math.round(94);
-    newStory = newStory.replace("300 pounds", weight);
-    newStory = newStory.replace("94 farenheit", temperature);
-  }
-
-  story.textContent = newStory;
-  story.style.visibility = "visible";
 }
