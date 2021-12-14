1fe75528142b6d6b9ad8923218c37822b9e9a764
Sergio <sergjimedaw10@gmail.com>
2021-12-14 09:21:48 +0100

    http

--- Módulo 2 - PROGRAMACIÓN WEB EN EL ENTORNO DEL SERVIDOR/node/referencia/http_module.js
+++ Módulo 2 - PROGRAMACIÓN WEB EN EL ENTORNO DEL SERVIDOR/node/referencia/http_module.js
@@ -1,10 +1,21 @@
-const http = require('http')
+const http = require("http");
+const fs = require("fs");
 
 //Crear un servidor
-http.createServer((req,res) => {
+http
+  .createServer((req, res) => {
     //Escribir una respuesta al recibir una petición
-    res.write('Helloo world from Nodejs http module')
-    res.end()
+    /*     res.write('Helloo world from Nodejs http module')
+    res.end() */
 
+    //Creamos el stream, de lectura
+    const myReadStream = fs.createReadStream(__dirname + "/data/data.txt", "utf-8");
+
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    // res.write("<h1>Helloo world from Nodejs http module</h1>");
+    // res.end();
+    //Respuesta en el stream de la response
+    myReadStream.pipe(res);
     // console.log(req)
-}).listen(5001, () => console.log('Servidor arrancado..'))
+  })
+  .listen(5000, () => console.log("Servidor arrancado.."));
