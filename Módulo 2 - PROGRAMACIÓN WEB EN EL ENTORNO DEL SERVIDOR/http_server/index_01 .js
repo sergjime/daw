6b3018d212500bd193137049f390f91b683fb529
Sergio <sergjimedaw10@gmail.com>
2021-12-14 13:20:19 +0100

    http node

---
+++ Módulo 2 - PROGRAMACIÓN WEB EN EL ENTORNO DEL SERVIDOR/http_server/index_01 .js
@@ -0,0 +1,15 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Home Mi Home</h1>");
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, console.log(`Servidor escuchando en el puerto ${PORT}`));
