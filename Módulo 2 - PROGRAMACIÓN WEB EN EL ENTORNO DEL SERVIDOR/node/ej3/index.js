b4cf754a681b997af44a23905035aaf99720f147
Sergio <sergjimedaw10@gmail.com>
2021-12-16 09:23:34 +0100

    Ejercicio 3

---
+++ Módulo 2 - PROGRAMACIÓN WEB EN EL ENTORNO DEL SERVIDOR/node/ej3/index.js
@@ -0,0 +1,4 @@
+console.debug('Hola Mundo de Node');
+console.info('Información de último momento, Node.js es lo más!!');
+console.warn('Tirando warnings como campeones');
+console.error('Algo no está bien!!!');
